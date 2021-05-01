aa2454619f400c4e357a7abafb27a6e68b4fef09
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2021-05-01 19:43:40 -0400

    installed axios, cors, and nodemail, included ContactForm compontent, adding gmail forwarding and receiving

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(5000, () => console.log("Server Running"));
+
+const contactEmail = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 993,
+    auth: {
+        user: "testadenevill@gmail.com",
+        pass: "IbuildAtlantis210",
+    },
+});
+
+contactEmail.verify((error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("Ready to Send");
+    }
+});
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import ContactForm from './ContactForm'
 
 export default function Contact() {
     return (
         <div>
             <h1>News Letter</h1>
+            <ContactForm>Here</ContactForm>
         </div>
+
     )
 }
